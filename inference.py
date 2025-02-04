d22550a8e7f72f89f99be68d1b3364a058805c4e
MetaPulkit <pulkit.agrawal@metacube.com>
2025-02-04 22:38:15 +0530

    commmittt

--- inference.py
+++ inference.py
@@ -7,9 +7,9 @@ import json
 
 def model_fn(model_dir):
     """Load the trained model from the model directory."""
-    model = joblib.load(os.path.join(model_dir, 'iris_model.joblib'))
+    model = joblib.load(os.path.join(model_dir, 'iris_model.tar.gz'))
     return model
-    
+
 def predict_fn(input_data, model):
     """Make prediction using the trained model."""
     input_data = np.array(input_data['instances'])
