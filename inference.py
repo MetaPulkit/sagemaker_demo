afb1b40cdc8b19204e985c5051865574378ff88e
MetaPulkit <pulkit.agrawal@metacube.com>
2025-05-12 14:57:14 +0530

    hi change

--- inference.py
+++ inference.py
@@ -22,6 +22,7 @@ def input_fn(request_body, content_type='application/json'):
     # Parse the input data
     if content_type == 'application/json':
         print(content_type)
+        print(content_type)
         return json.loads(request_body)
     else:
         raise ValueError("Unsupported content type")
